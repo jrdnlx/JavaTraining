bd6c608a87c02812b4094f455591a548f7e3d054
jrdnlx <jordansie13@gmail.com>
2022-07-25 15:19:02 -0400

    Assignments

--- AS2/AS2/src/main/java/com/hcl/AS3/Vehicle.java
+++ AS2/AS2/src/main/java/com/hcl/AS3/Vehicle.java
@@ -2,6 +2,15 @@ package com.hcl.AS3;
 
 public class Vehicle {
 
+	int vehicleNo;
+	String manufactorer;
+	String color;
+	
+	class Truck extends Vehicle{
+		
+		int loadingCapacity;
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
