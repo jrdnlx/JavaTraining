425e025bad597bf161b2d876762179a7a2c21428
jrdnlx <jordansie13@gmail.com>
2022-07-25 15:45:59 -0400

    AS3-5

--- AS2/AS2/src/main/java/com/hcl/AS4/ReverseList.java
+++ AS2/AS2/src/main/java/com/hcl/AS4/ReverseList.java
@@ -1,10 +1,24 @@
 package com.hcl.AS4;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ReverseList {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		List<Integer> list = new ArrayList<>();
+		
+		
+		for(int i = 0; i < 10; i++) {
+			list.add(i);
+		}
+		
+		Collections.reverse(list);
+		
+		list.forEach(System.out::println);
 	}
 
 }
---
+++ AS2/AS2/src/main/java/com/hcl/AS5/Employee.java
@@ -0,0 +1,21 @@
+package com.hcl.AS5;
+
+public class Employee {
+
+	int id;
+	String name;
+	int age;
+	String gender;
+	String dept;
+	int yearJoined;
+	int salary;
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}
